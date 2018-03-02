fa2f2de92de866afd42e06f642db668c3da2d4f6
Shelton <trulyshelton@users.noreply.github.com>
2018-03-01 17:26:21 -0800

    dummy test.py updated

--- test.py
+++ test.py
@@ -2,8 +2,17 @@
 import random, time
 
 
-x = random.randint(0,180)
+get_speed = lambda : random.randint(0,180)
+get_light = lambda : random.choice(['L', 'N', 'R'])
+get_gear = lambda : random.choice(['P', 'R', 'N', 'D'])
+get_acc = lambda : random.uniform(0, 1)
+get_break = lambda : random.uniform(0, 1)
+
+
 while True:
-    print('speed:%i' % x, flush=True)
-    x = random.randint(0, 180)
+    print('speed:%i' % get_speed(), flush=True)
+    print('light:%s' % get_light(), flush=True)
+    print('gear:%s' % get_gear(), flush=True)
+    print('accelerator:%f' % get_acc(), flush=True)
+    print('break:%f' % get_break(), flush=True)
     time.sleep(1)
